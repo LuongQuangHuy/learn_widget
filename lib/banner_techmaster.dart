c49a22f515ee739a2bfb7bd8b5bd44777da79717
Hoang Nguyen <hoang@nta.io>
2020-05-09 20:22:04 +0700

    font set up

---
+++ lib/banner_techmaster.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+class BannerTechmaster extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text("sas",style: TextStyle(fontFamily: "Montserrat"),),);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learnwidget/banner_techmaster.dart';
 
 void main() => runApp(Cats());
 
@@ -11,17 +12,17 @@ class Cats extends StatelessWidget {
         appBar: AppBar(
           title: Text('Cats'),
         ),
-        body: Center(
+        body: BannerTechmaster()/*Center(
           child: Container(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Row(
                   children: <Widget>[
-                  margin: EdgeInsets.all(15.0),
-                  Container(width: 50, height: 50, Image.network('https://www.pexels.com/vi-vn/anh/c-n-c-nh-con-meo-con-v-t-d-th-ng-730896/'),),
-                    Container(width: 50, height: 50, Image.network('https://www.pexels.com/vi-vn/anh/c-n-c-nh-con-meo-con-v-t-d-th-ng-617278/'),),
-                    Container(width: 50, height: 50, Image.network('https://www.pexels.com/vi-vn/anh/con-meo-con-v-t-d-th-ng-loai-v-t-45201/'),),
+
+                  Container(width: 50, height: 50,child: Image.network('https://www.pexels.com/vi-vn/anh/c-n-c-nh-con-meo-con-v-t-d-th-ng-730896/'),),
+                    Container(width: 50, height: 50, child:Image.network('https://www.pexels.com/vi-vn/anh/c-n-c-nh-con-meo-con-v-t-d-th-ng-617278/'),),
+                    Container(width: 50, height: 50, child:Image.network('https://www.pexels.com/vi-vn/anh/con-meo-con-v-t-d-th-ng-loai-v-t-45201/'),),
 
                     Image.network("https://www.pexels.com/vi-vn/anh/con-meo-con-v-t-d-th-ng-loai-v-t-45201/"),
                   ],
@@ -29,26 +30,24 @@ class Cats extends StatelessWidget {
                 ),
                 Row(
                   children: <Widget>[
-                    margin: EdgeInsets.all(15.0),
-                    Container(width: 50, height: 50, Image.network('https://www.pexels.com/vi-vn/anh/ch-p-nh-d-ng-v-t-con-meo-con-v-t-d-th-ng-57416/')),
-                    Container(width: 50, height: 50, Image.network('https://www.pexels.com/vi-vn/anh/c-n-c-nh-con-meo-con-v-t-d-th-ng-320014/')),
-                    Container(width: 50, height: 50 ,Image.network('https://www.pexels.com/vi-vn/anh/au-y-m-bu-n-c-i-chan-dung-con-meo-209037/')),
+                    Container(width: 50, height: 50, child: Image.network('https://www.pexels.com/vi-vn/anh/ch-p-nh-d-ng-v-t-con-meo-con-v-t-d-th-ng-57416/')),
+                    Container(width: 50, height: 50, child: Image.network('https://www.pexels.com/vi-vn/anh/c-n-c-nh-con-meo-con-v-t-d-th-ng-320014/')),
+                    Container(width: 50, height: 50 ,child: Image.network('https://www.pexels.com/vi-vn/anh/au-y-m-bu-n-c-i-chan-dung-con-meo-209037/')),
                   ],
                   mainAxisAlignment: MainAxisAlignment.center,
                 ),
                 Row(
                   children: <Widget>[
-                    margin: EdgeInsets.all(15.0),
-                    Container(width: 50, height: 50, Image.network('https://www.pexels.com/vi-vn/anh/c-n-c-nh-chan-dung-c-con-meo-1472999/')),
-                    Container(width: 50, height: 50,Image.network('https://www.pexels.com/vi-vn/anh/c-n-c-nh-chan-con-meo-con-v-t-69932/')),
-                    Container(width: 50, height: 50, Image.network('https://www.pexels.com/vi-vn/anh/cai-d-u-c-n-c-nh-ch-p-nh-d-ng-v-t-con-meo-2071873/')),
+                    Container(width: 50, height: 50, child: Image.network('https://www.pexels.com/vi-vn/anh/c-n-c-nh-chan-dung-c-con-meo-1472999/')),
+                    Container(width: 50, height: 50,child: Image.network('https://www.pexels.com/vi-vn/anh/c-n-c-nh-chan-con-meo-con-v-t-69932/')),
+                    Container(width: 50, height: 50, child: Image.network('https://www.pexels.com/vi-vn/anh/cai-d-u-c-n-c-nh-ch-p-nh-d-ng-v-t-con-meo-2071873/')),
                   ],
                   mainAxisAlignment: MainAxisAlignment.center,
                 ),
               ],
             ),
           ),
-        ),
+        ),*/
       ),
     );
   }
